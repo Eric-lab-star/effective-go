fda2f906ecb77be23f738037ec29d6b00ecf22f1
eric-lab-star <cyon256@icloud.com>
2023-05-21 13:21:06 +0900

    clear

--- path.go
+++ path.go
@@ -1,83 +1,14 @@
-/*
-Gofmt formats Go programs.
-It uses tabs for indentation and blanks for alignment.
-Alignment assumes that an editor is using a fixed-width font.
+package main
 
-Without an explicit path, it processes the standard input. Given a file,
-it operates on that file; given a directory, it operates on all .go files in
-that directory, recursively. (Files starting with a period are ignored.)
-By default, gofmt prints the reformatted sources to standard output.
+import "fmt"
 
-Usage:
-
-	gofmt [flags] [path ...]
-
-The flags are:
-
-	-d
-	    Do not print reformatted sources to standard output.
-	    If a file's formatting is different than gofmt's, print diffs
-	    to standard output.
-	-w
-	    Do not print reformatted sources to standard output.
-	    If a file's formatting is different from gofmt's, overwrite it
-	    with gofmt's version. If an error occurred during overwriting,
-	    the original file is restored from an automatic backup.
-
-When gofmt reads from standard input, it accepts either a full Go program
-or a program fragment. A program fragment must be a syntactically
-valid declaration list, statement list, or expression. When formatting
-such a fragment, gofmt preserves leading indentation as well as leading
-and trailing spaces, so that individual sections of a Go program can be
-formatted by piping them through gofmt.
-*/
-package Class
-
-// Quote returns a double-quoted Go string literal representing s.
-// The returned string uses Go escape sequences (\t, \n, \xFF, \u0100)
-// for control characters and non-printable characters as defined by IsPrint.
-func Quote(s string) string {
-	return s
+type shape struct {
+	vartex int
+	color  string
+	angle  int
 }
 
-// Student describes name of student
-type Student struct {
-	Name string
+func main() {
+	text := []rune{104, 101, 108, 108, 111}
+	fmt.Println(string(text))
 }
-
-// A LimitedReader reads from R but limits the amount of
-// data returned to just N bytes. Each call to Read
-// updates N to reflect the new amount remaining.
-// Read returns EOF when N <= 0.
-type LimitedReader struct {
-	R Reader // underlying reader
-	N int64  // max bytes remaining
-}
-
-// Copy copies from src to dst until either EOF is reached
-// on src or an error occurs. It returns the total number of bytes
-// written and the first error encountered while copying, if any.
-//
-// A successful Copy returns err == nil, not err == EOF.
-// Because Copy is defined to read from src until EOF, it does
-// not treat an EOF from Read as an error to be reported.
-func Copy(dst Writer, src Reader) (n int64, err error) {
-
-}
-
-type Buffer struct {
-	// Has unexported fields.
-}
-
-//    A Buffer is a variable-sized buffer of bytes with Read and Write methods.
-//   The zero value for Buffer is an empty buffer ready to use.
-
-func NewBuffer(buf []byte) *Buffer
-func NewBufferString(s string) *Buffer
-func (b *Buffer) Bytes() []byte
-func (b *Buffer) Cap() int
-func (b *Buffer) Grow(n int)
-func (b *Buffer) Len() int
-func (b *Buffer) Next(n int) []byte
-func (b *Buffer) Read(p []byte) (n int, err error)
-func (b *Buffer) ReadByte() (byte, error)
